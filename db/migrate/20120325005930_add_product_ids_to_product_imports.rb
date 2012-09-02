7c13541268045d5a48529937a6f75f367360cf75
Rashit Azizbaev <syndicut@gmail.com>
2012-09-02 14:42:51 +0400

    Removed namespace from migrations

--- db/migrate/20120325005930_add_product_ids_to_product_imports.rb
+++ db/migrate/20120325005930_add_product_ids_to_product_imports.rb
@@ -1,5 +1,5 @@
 class AddProductIdsToProductImports < ActiveRecord::Migration
   def change
-    add_column  :spree_product_imports, :product_ids, :text
+    add_column  :product_imports, :product_ids, :text
   end
 end
