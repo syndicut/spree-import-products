3affd427bf184c8d1bcb34a8f7e3489cbc43f749
Rashit Azizbaev <syndicut@gmail.com>
2012-09-02 16:27:35 +0400

    Fixed url helpers for spree 0.70

--- spec/requests/import_products_spec.rb
+++ spec/requests/import_products_spec.rb
@@ -6,7 +6,7 @@ feature "Import products" do
   end
 
   scenario "admin should be able to import products and delete import" do
-    visit spree.admin_product_imports_path
+    visit admin_product_imports_path
     attach_file("product_import_data_file", File.join(File.dirname(__FILE__), '..', 'fixtures', 'valid.csv'))
     click_button "Create"
 
@@ -16,10 +16,10 @@ feature "Import products" do
     Delayed::Worker.new.work_off
 
     # should have created the product
-    visit spree.admin_products_path
+    visit admin_products_path
     page.should have_content("Bloch Kids Tap Flexww")
 
-    visit spree.admin_product_imports_path
+    visit admin_product_imports_path
     page.should have_content("valid.csv")
     page.should have_content("Completed")
 
@@ -31,7 +31,7 @@ feature "Import products" do
     page.should_not have_content("valid.csv")
 
     # should have deleted product created by import
-    visit spree.admin_products_path
+    visit admin_products_path
     page.should_not have_content("Bloch Kids Tap Flexww")
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,5 +32,4 @@ RSpec.configure do |config|
   # examples within a transaction, comment the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
-  config.include Spree::UrlHelpers
 end
